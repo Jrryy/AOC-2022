043c7086ad6bfd8689fa9e20b9b2aa23a3d52274
Jrryy <jerry.om.95@gmail.com>
2022-12-30 14:39:11 +0100

    Make it able to run only one day

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"AOC-2022/pkg/challenges"
+	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -26,14 +28,32 @@ var days = []func(){
 }
 
 func main() {
-	totalElapsed := time.Duration(0)
-	for day, challenge := range days {
-		fmt.Printf("Day %02d\n", day+1)
+	dayFlag := flag.Int(
+		"d",
+		0,
+		fmt.Sprintf("The day of the challenge you want to run, from 1 to %d", len(days)),
+	)
+	flag.Parse()
+	if *dayFlag == 0 {
+		totalElapsed := time.Duration(0)
+		for day, challenge := range days {
+			fmt.Printf("Day %02d\n", day+1)
+			start := time.Now()
+			challenge()
+			elapsed := time.Since(start)
+			fmt.Printf("Elapsed: %v\n", elapsed)
+			totalElapsed += elapsed
+		}
+		fmt.Printf("Total elapsed time: %v\n", totalElapsed)
+	} else if *dayFlag > len(days) {
+		println("The challenge for this day has not yet been solved.")
+		os.Exit(1)
+	} else if *dayFlag < 0 {
+		fmt.Printf("Please input a positive integer from 1 to %d\n", len(days))
+		os.Exit(1)
+	} else {
 		start := time.Now()
-		challenge()
-		elapsed := time.Since(start)
-		fmt.Printf("Elapsed: %v\n", elapsed)
-		totalElapsed += elapsed
+		days[*dayFlag-1]()
+		fmt.Printf("Elapsed: %v\n", time.Since(start))
 	}
-	fmt.Printf("Total elapsed time: %v\n", totalElapsed)
 }
